6a74b1bcbebd4bc4fd7cb09d52479c6e552a1037
SheliVas <sheli.vassermans@post.runi.ac.il>
2023-05-13 10:21:49 +0300

    Added quickSort and insertionSort methods, still missing implementation

---
+++ hw5/src/Sort.java
@@ -0,0 +1,35 @@
+public class Sort<T extends Comparable<T>> {
+    
+
+    public void quickSort(T[] array) {
+      // call for sorting the array.  
+    }
+
+    private void quickSort(T[] array, int low, int high) {
+     if (high - low > 2) {
+        int pivot = partition(array, low, high);
+        quickSort(array, low, pivot - 1);
+        quickSort(array, pivot + 1, high);
+     } else {
+        insertionSort(array, low, high);
+     }
+    }
+    
+    public void quickSortRecitation(T[] array) {
+        // Implementation goes here
+    }
+
+    // explain why insertion sort
+    private void insertionSort(T[] array, int low, int high) {
+        for (int i = low; i <= high; i++) {
+            T val = array[i];
+            int j = i-1;
+            while (j >= low && array[j].compareto(val) > 0) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = val;
+        }
+    }
+    
+}
